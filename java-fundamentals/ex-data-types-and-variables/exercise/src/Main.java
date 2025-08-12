0ec40a210c371e30da983367338c4bd65def5070
aorlando-apprenti <aorlando@apprenticareers.org>
2025-08-12 16:36:30 -0400

    AO: Pushing Exercise Starter Code for Datatypes and Variables

---
+++ java-fundamentals/ex-data-types-and-variables/exercise/src/Main.java
@@ -0,0 +1,55 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Exercise Code Starts Here:");
+
+
+        //Sports Statistics
+        //1 Declare and assign the following variables related to a soccer player:
+        //2 playerName → store the name of a soccer player.
+        String playerName = "Mark Messi";
+        //3 jerseyNumber → store the player's jersey number.
+        //4 position → store the player’s field position (e.g., "Goalkeeper").
+        //5 isStarter → store whether the player is a starter (true or false).
+        //6 teamName → store the name of the player’s team.
+
+
+        //Movie Information
+        //1 Declare and assign the following variables for a movie:
+        //2 movieTitle → store the title of a movie.
+        //3 releaseYear → store the year the movie was released.
+        //4 rating → store the rating (e.g., "PG-13").
+        //5 isSequel → store whether the movie has a sequel (true or false).
+        //6 leadActor → store the name of the lead actor.
+
+
+        //Weather Report
+        //1. Declare and assign the following variables for a weather forecast:
+        //2. cityName → store the name of a city.
+        //3. temperature → store the temperature in Fahrenheit.
+        //4. isRaining → store whether it is currently raining (true or false).
+        //5. humidity → store the humidity percentage.
+        //6. weatherCondition → store a short description of the weather (e.g. "Cloudy").
+
+
+
+        //Flight Information
+        //1 Declare and assign the following variables for a flight at an airport:
+        //2 flightNumber → store the flight number (e.g., "AA256").
+        //3 departureCity → store the departure city (e.g., "New York").
+        //4 arrivalCity → store the arrival city (e.g., "Los Angeles").
+        //5 gateNumber → store the gate number for the flight.
+        //6 terminal → store the terminal number at the airport.
+        //7 isDelayed → store whether the flight is delayed (true or false).
+
+
+
+        //Part 2: Printing Variables
+        // Use System.out.println() to print each set of variables.
+        // Format the output to display meaningful sentences
+        System.out.println("Player Name = " + playerName);
+
+
+    }
+}
