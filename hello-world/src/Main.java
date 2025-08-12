7b83e9b2ddab7eea8d50f08b3ed7876344fd4e73
aorlando-apprenti <aorlando@apprenticareers.org>
2025-08-12 12:06:28 -0400

    AO: Pushing Hello World

---
+++ hello-world/src/Main.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("Hello and welcome!");
+
+
+
+        for (int i = 1; i <= 10; i++) {
+            if (i % 2 == 0) {
+                System.out.println("i = " + i);
+            }
+        }
+    }
+}
