8257a59663e78ed99c4e994509fd0d468b74d61b
aorlando-apprenti <aorlando@apprenticareers.org>
2025-08-19 15:12:40 -0400

    AO: Pushing Loops Shopping  Cart Exercise

---
+++ java-fundamentals/exercise-shopping-cart-loops-starter-code/src/main/java/org/example/shoppingcart/ShoppingCartApp.java
@@ -0,0 +1,44 @@
+package org.example.shoppingcart;
+
+import java.util.Scanner;
+
+public class ShoppingCartApp {
+  public static void main(String[] args) {
+    Scanner console = new java.util.Scanner(System.in);
+
+    System.out.println("Welcome to the shopping cart app!");
+
+    boolean confirm = false;
+
+    String taxExempt = "";
+    String shipping = "";
+    String promoCode = "";
+    int orderQuantity = 0;
+
+    // Prompt for tax exempt
+    System.out.println("Are you tax-exempt? (y/n)");
+    taxExempt = console.nextLine();
+
+    // Prompt for shipping
+    System.out.println("Shipping? (standard/overnight/twoday)");
+    shipping = console.nextLine();
+
+    // Prompt for order quantity
+    System.out.println("Order quantity?");
+    orderQuantity = Integer.parseInt(console.nextLine());
+
+    // Prompt for promo code
+    System.out.println("Promo code for free shipping?");
+    promoCode = console.nextLine();
+
+    // Print details
+    System.out.println("\nDetails:");
+    System.out.println("Tax-exempt: " + taxExempt);
+    System.out.println("Shipping: " + shipping);
+    System.out.println("Order quantity: " + orderQuantity);
+    System.out.println("Promo code: " + promoCode);
+
+
+    System.out.println("Bye");
+  }
+}
