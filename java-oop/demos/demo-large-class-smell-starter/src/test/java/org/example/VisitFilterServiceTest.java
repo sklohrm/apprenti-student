24cc85b64956abfa4f8e5fe784bf43647d21a79a
aorlando-apprenti <aorlando@apprenticareers.org>
2025-09-02 09:41:45 -0400

    AO: Pushing Large Class Smell Demo

---
+++ java-oop/demos/demo-large-class-smell-starter/src/test/java/org/example/VisitFilterServiceTest.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class VisitFilterServiceTest {
+    VisitFilterService visitFilterService = new VisitFilterService();
+    Visit samplePatientVisit;
+    Visit[] visitList;
+
+    @BeforeEach
+    void setUp() {
+        samplePatientVisit = TestData.getExamplePatientVisit();
+        visitList = TestData.getPatientVisits();
+    }
+    @Test
+    void getVisitsByProviderId() {
+        int expectedSize = 2;
+        assertEquals(expectedSize, visitFilterService.getVisitsByProviderId("ZXY21", TestData.getPatientVisits()).length);
+    }
+
+    @Test
+    void getVisitsByPatientId() {
+        int expectedSize = 2;
+        assertEquals(expectedSize, visitFilterService.getVisitsByPatientId("ABC123", TestData.getPatientVisits()).length);
+    }
+}
