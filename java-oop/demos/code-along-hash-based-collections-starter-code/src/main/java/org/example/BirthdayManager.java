142bf4f496e56e4729efa8febf9a9663bdcf2149
aorlando-apprenti <aorlando@apprenticareers.org>
2025-09-08 16:04:30 -0400

    AO: Pushing Hash Based Collections Code Along

---
+++ java-oop/demos/code-along-hash-based-collections-starter-code/src/main/java/org/example/BirthdayManager.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BirthdayManager {
+    public List<Friend> getFriends() {
+        return new ArrayList<Friend>();
+    }
+
+    public void addFriend(Friend newFriend) {
+
+    }
+
+    public void addGift(Friend friend, Gift gift) {
+
+    }
+
+    public void removeFriend(Friend exFriend) {
+
+    }
+
+
+
+
+}
