3a076f5e7612e0473a6b96ab8d0e8d4ec52b469d
aorlando-apprenti <aorlando@apprenticareers.org>
2025-10-09 12:08:35 -0400

    AO-Pushing Starter Code for Subqueries

---
+++ databases/exercises/exercise-subqueries-starter.sql
@@ -0,0 +1,21 @@
+-- Part 1: Using NOT EXISTS
+--  Write a query to list all Students that are not registerd for a class 
+-- Hint(Check the Section Roster) 
+
+-- Part 2: Subqueries in the WHERE Clause
+-- Find the name(s) of classes that have the most students registered
+
+-- Part 3: Subquery in the SELECT Clause
+-- Select all Classes Show:
+-- The Teacher That is assigned to Instruct
+-- The Room That the class is in
+-- No Joins Allowed
+
+-- Part 4: Subquery in the HAVING Clause
+-- Use the Query from Part 2. Use Having to determine which class(s)
+-- Has the least amount of Students (Must Be in a Subquery)
+
+
+-- Part 5: Correlated Subquery
+-- Return the top 3 Teachers that have the most students to teach 
+-- across all classes and semesters
