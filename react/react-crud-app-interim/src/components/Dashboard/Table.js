4991eecaa0fb8fcfc186770a8627b23a91452174
aorlando-apprenti <aorlando@apprenticareers.org>
2025-10-28 15:47:07 -0400

    Pushing React CRUD APP Interim

---
+++ react/react-crud-app-interim/src/components/Dashboard/Table.js
@@ -0,0 +1,69 @@
+import React from 'react';
+
+const Table = ({ employees, handleEdit, handleDelete }) => {
+   
+    employees.forEach((employee, i) => {
+        employee.id = i + 1;
+    });
+
+    const formatter = new Intl.NumberFormat('en-US',{
+        style: 'currency',
+        currency: 'USD',
+        minimumFractionDigits: null,
+    });
+
+    return (
+        <div className = "contain-table">
+            <table className="striped-table">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Email</th>
+                        <th>Salary</th>
+                        <th>Date</th>
+                        <th colspan={2} className="text-center">
+                            Actions
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {employees.length > 0 ? (
+                        employees.map((employee, i) => (
+                            <tr key={employee.id}>
+                                <td>{i+1}</td>
+                                <td>{employee.firstName}</td>
+                                <td>{employee.lastName}</td>
+                                <td>{employee.email}</td>
+                                <td>{formatter.format(employee.salary)}</td>
+                                <td>{employee.date}</td>
+                                <td className = "text-right">
+                                    <button
+                                        onClick={() => handleEdit(employee.id)}
+                                        className="button muted-button"
+                                    >
+                                    Edit    
+                                    </button>
+                                    <button
+                                        onClick={() => handleDelete(employee.id)}
+                                        className="button muted-button"
+                                    >
+                                    Delete    
+                                    </button>
+                                </td>
+                            </tr>
+                        )) 
+                    ) : (
+                        <tr>
+                            <td colspan={7}>No Employees to Display</td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
+        </div>
+    );
+
+};
+
+export default Table;
